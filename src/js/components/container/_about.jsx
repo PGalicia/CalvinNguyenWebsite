d64d51c00eac258005191b079d407820be9027be
PGalicia <galicia.patrick@gmail.com>
2019-11-08 20:25:05 -0800

    Use webpack resolve alias

---
+++ src/js/components/container/_about.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>About page</h1>;
+  }
+}
+
+export default About;
--- src/js/components/container/main.jsx
+++ src/js/components/container/main.jsx
@@ -8,12 +8,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "@container/_home.jsx";
+import About from "@container/_about.jsx";
 
 const Main = props => {
   return (
     <main id="main">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </Switch>
     </main>
   );
