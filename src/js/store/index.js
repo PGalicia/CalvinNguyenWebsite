6bb729a1f6a916e67e8bf2fefeaec325f2fd5194
PGalicia <galicia.patrick@gmail.com>
2019-11-17 15:56:33 -0800

    Remove react middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,17 +1,11 @@
 import {
-    createStore,
-    applyMiddleware,
-    compose
+    createStore
 } from "redux";
-// import { forbiddenWordsMiddleware } from "../middleware/index.js.js.js";
-// import rootReducer from "./../reducers/index.js";
+
 import rootReducer from "@reducer";
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
-    rootReducer,
-    // storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+    rootReducer
 );
 
 export default store;
